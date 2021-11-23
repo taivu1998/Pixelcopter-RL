3b64146a92c80ab468f17371e34a2e385a47c716
taivu1998 <46636857+taivu1998@users.noreply.github.com>
2021-11-23 00:00:43 -0800

    Add trainer

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,2 +1,3 @@
 from .random_model import Random
 from .qlearning import QLearning
+from .trainer import Trainer
--- agents/trainer.py
+++ agents/trainer.py
@@ -1,17 +1,35 @@
 from .random_model import Random
 from .qlearning import QLearning
+from game import GameMDP, DiscretizedGameMDP
 
-model_names = {
+MODEL_TYPES = {
     'random': Random,
     'qlearning': QLearning,
 }
-class QLearning(object):
+
+MDP_TYPES = {
+    'normal': GameMDP,
+    'discretized': DiscretizedGameMDP,
+}
+class Trainer(object):
 
     def __init__(self, args=None):
         self.args = args
+        if 'grid_size' in self.args and self.args['grid_size'] > 0:
+            game = MDP_TYPES[self.args['mdp_type']](grid_size=self.args['grid_size'])
+        else:
+            game = MDP_TYPES[self.args['mdp_type']]()
+        self.model = MODEL_TYPES[self.args['model_type']](game=game, args=self.args)
 
     def train(self):
-        return
+        self.model.train()
 
     def evaluate(self):
+        scores = self.model.evaluate(epochs=self.args['eval_epochs'])
+        return scores
+
+    def save_scores(self):
+        return
+
+    def save_q_values(self):
         return
